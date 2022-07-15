5d2b3b56093317c905b8a87f809fbde1b71c9909
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-07-15 13:21:42 -0500

    Add a second way to solve subsets.

--- first-50/78_subsets.js
+++ first-50/78_subsets.js
@@ -17,6 +17,25 @@ Input: nums = [1,2,3]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 */
 
+// easier way to solve subsets?
+let subsets2 = (nums) => {
+  let res = [];
+  let helper = (res, nums, curr, i) => {
+    if (i === nums.length) {
+      res.push([...curr]);
+      return;
+    }
+
+    helper(res, nums, curr, i + 1);
+    helper(res, nums, [...curr, nums[i]], i + 1);
+  };
+  helper(res, nums, [], 0);
+  console.log(res);
+  return res;
+};
+
+subsets2([1, 2, 3]);
+
 var subsets = function (nums) {
   let res = [];
 
@@ -36,4 +55,4 @@ var subsets = function (nums) {
   return res;
 };
 
-subsets([1, 2, 3]);
+// subsets([1, 2, 3]);
